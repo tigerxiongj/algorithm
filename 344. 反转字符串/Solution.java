48293c74fc32215299dc42112d3b92e1124ebc54
tiger.xiong <tiger.xiong@unisoc.com>
2019-08-12 13:34:12 +0800

    反转字符串

---
+++ 344. 反转字符串/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+	
+	/**
+	 * Õ∑Œ≤÷∏’Î
+	 * @param s
+	 */
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while (i<j) {
+        	char t = s[i];
+        	s[i] = s[j];
+        	s[j] = t;
+        	i++;
+        	j--;
+        }
+    }
+}
